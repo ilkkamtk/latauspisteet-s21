f65831022300d4d48af9ea66a0f84b54885a5cbc
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2021-11-22 11:10:00 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -30,6 +30,13 @@ const options = {
 
 // funktio markerien tekoon
 function lisaaMarker(latitude, longitude, teksti, ikoni, info, origin) {
+  const googleOsoite = 'https://www.google.com/maps/dir/?api=1';
+  const parametrit = `&origin=${origin.latitude},${origin.longitude}&destination=${latitude},${longitude}&travelmode=driving`;
+
+  teksti = `<h3>${teksti}</h3>`;
+  teksti += `<div>
+                <a href="${googleOsoite + parametrit}" target="_blank">Navigoi</a>
+            </div>`
   L.marker([latitude, longitude], {icon: ikoni}).
       addTo(map).
       bindPopup(teksti).
@@ -39,8 +46,6 @@ function lisaaMarker(latitude, longitude, teksti, ikoni, info, origin) {
         kaupunki.innerHTML = info?.kaupunki || '';
         lisatiedot.innerHTML = info?.lisatiedot || '';
 
-        const googleOsoite = 'https://www.google.com/maps/dir/?api=1';
-        const parametrit = `&origin=${origin.latitude},${origin.longitude}&destination=${latitude},${longitude}&travelmode=driving`;
         navigoi.href = googleOsoite + parametrit;
       });
 }
